d2ab8ad76d15ce310067b8b39ce630ac0a8f5b95
Yonathan Tulu <yonathan.tulu@gmail.com>
2022-12-25 20:30:27 -0600

    Add Profiles, and Profiles View.

--- App.js
+++ App.js
@@ -1,19 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { NavigationContainer, DarkTheme } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { LensProvider } from '@lens-protocol/react-native-lens-ui-kit';
+
+import Profiles from './Profiles'
+import ViewProfile from './ProfileView'
+
+
+const Stack = createNativeStackNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <LensProvider theme="dark">
+      <NavigationContainer theme={DarkTheme}>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={Profiles}  />
+          <Stack.Screen name="ViewProfile" component={ViewProfile} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </LensProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ Profiles.js
@@ -0,0 +1,19 @@
+import {
+    Profiles
+   } from '@lens-protocol/react-native-lens-ui-kit'
+    
+   const App = ({
+     navigation
+   }) => {
+     return (
+         <Profiles
+           onProfilePress={
+             profile => navigation.navigate(
+               "ViewProfile", { profile }
+             )
+           }
+         />    
+     );
+   }
+   
+   export default App;
